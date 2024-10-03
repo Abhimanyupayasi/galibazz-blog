eb536f5d65e370a4a66a2e39aa004c9498c0d805
Abhimanyu <abhimanyupayasi01@gmail.com>
2024-10-03 23:37:24 +0530

    add some file - dirty jokes & posters

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./*.{html,js}"
+    , "./hi/*.{html,js}",
+    "./hi/*/*.{html,js}",
+    "./hi/*/*/*.{html,js}",
+    "./en/*.{html,js}",
+    "./en/*/*.{html,js}",
+    "./en/*/*/*.{html,js}",
+  ],
   theme: {
     extend: {},
   },
